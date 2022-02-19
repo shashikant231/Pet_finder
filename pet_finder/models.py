6ca7d09fc33c6024b5c9452c4e84e2388ab1f337
shashikant231 <shashikantching@gmail.com>
2022-02-20 01:28:52 +0530

    bug solved

--- pet_finder/models.py
+++ pet_finder/models.py
@@ -136,7 +136,7 @@ class Pet(models.Model):
     size = models.CharField(choices=size_choices,max_length=10,blank=False)
     vaccination_choices = (("Not Vaccianted", "Not Vaccianted"), 
                             ("Partially Vaccinated", "Partially Vaccinated"),
-                            ("Completely vaccinated","Completely Vaccinated"))
+                            ("Completely Vaccinated","Completely Vaccinated"))
     vaccination = models.CharField(choices=vaccination_choices,max_length=30,blank=False)
     first_image = models.ImageField(upload_to ='media/')
     second_image = models.ImageField(upload_to ='media/',blank = True)
